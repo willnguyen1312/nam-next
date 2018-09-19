89f9401d1ef382531e5c63fa46267c28874a6e2e
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-09-19 12:13:18 +0700

    Remove enzyme

--- pages/cars.tsx
+++
@@ -1,6 +0,0 @@
-import CarsOverview from 'modules/cars/Overview'
-import * as React from 'react'
-
-const CarsPage = () => <CarsOverview />
-
-export default CarsPage
--- pages/login.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import Login from './../src/modules/auth/Login'
-
-const LoginPage = () => <Login />
-
-export default LoginPage
