0e79ccb3c93a7f484191c0b5c4e7ca19e8c7606d
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-02 23:34:12 +0700

    Add linting 🎉

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,12 @@ export default function IndexPage() {
       <Stack direction={["column"]} spacing="24px">
         <Button>Hello there</Button>
         <Box>
-          <Image src="https://picsum.photos/200" width={200} height={200} />
+          <Image
+            alt="Stuff"
+            src="https://picsum.photos/200"
+            width={200}
+            height={200}
+          />
         </Box>
         <Box>
           <Zeit />
