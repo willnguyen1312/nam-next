6bd4fb31c85ba68d9538c0781ee24b156f0a933f
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-10-02 22:16:33 +0700

    Add Bundle Analyzer 🎉

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withPlugins = require("next-compose-plugins");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withPlugins([[withBundleAnalyzer]], {
+  images: {
+    domains: ["picsum.photos"],
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
-import { Button, Container } from "@chakra-ui/react";
+import Image from "next/image";
+import { Button, Container, Stack, Box } from "@chakra-ui/react";
+import { Zeit } from "../components/icons";
 
 export default function IndexPage() {
   return (
     <Container>
-      <Button>Hello there</Button>
+      <Stack direction={["column"]} spacing="24px">
+        <Button>Hello there</Button>
+        <Box>
+          <Image src="https://picsum.photos/200" width={200} height={200} />
+        </Box>
+        <Box>
+          <Zeit />
+        </Box>
+      </Stack>
     </Container>
   );
 }
